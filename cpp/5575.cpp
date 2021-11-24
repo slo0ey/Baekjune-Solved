381f5333533df44d4119ad437d5bd6dbdd621ba8
slo0ey <rurchi1206@gmail.com>
2021-11-24 17:10:53 +0900

    5575 c++ 해결

---
+++ cpp/5575.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void serialize(int(&time)[3], int sec) {
+  time[0] = sec / 3600;
+  time[1] = (sec % 3600) / 60;
+  time[2] = ((sec % 3600) % 60);
+}
+
+void deserialize(int& time, int h, int m, int s) {
+  time = (h * 3600) + (m * 60) + s;
+}
+
+int main() {
+  int h1, m1, s1, h2, m2, s2;
+  int T1, T2;
+  int R[3];
+  for (int i = 0; i < 3; i++) {
+    std::cin >> h1 >> m1 >> s1 >> h2 >> m2 >> s2;
+    deserialize(T1, h1, m1, s1);
+    deserialize(T2, h2, m2, s2);
+    serialize(R, T2 - T1);
+    std::cout << R[0] << " " << R[1] << " " << R[2] << std::endl;
+  }
+}
